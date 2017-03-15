03032b241068156c8ce965fb1d8b3d86dfc89694
CProv93 <cprov93@uw.edu>
2017-03-15 15:17:46 -0700

    Add files via upload

--- airfoil_boundaries.m
+++ airfoil_boundaries.m
@@ -1,4 +1,4 @@
-function [Ub1] = airfoil_boundaries( I_MAX, J_MAX, U,S,X)
+function UB1 = airfoil_boundaries( I_MAX, J_MAX, U,S,X)
 
 %extrapolate each quantity
 gamma = 1.4;
@@ -12,7 +12,7 @@ Eslopes = (E(:,2) - E(:,1))./dist;
 
 u = U(:,1,2)./U(:,1,1);
 v = U(:,1,3)./U(:,1,1);
-Ub1 = zeros(I_MAX-1,4);
+UB1 = zeros(I_MAX-1,4);
 %tangential quantities for the surface
 ul = u.*S(:,1,3,2) - v.*S(:,1,3,1); 
 % now recompute the u and v for physical 
@@ -28,13 +28,13 @@ distin = distrat.*dist;
 %yin = distin.*sin(theta);
 
 %do the extrapolation of values 
-Pba = P(:,1) + ((distin - dist)./(dist2-dist)) * (P(:,2) - P(:,1));
-rhoba = U(:,1,1) + ((distin - dist)./(dist2-dist)) * (U(:,2,1) - U(:,1,1));
-Eba  = E(:,1) + ((distin - dist)./(dist2-dist)) * (E(:,2) - E(:,1));
+Pba = P(:,1) + ((distin - dist)./(dist2-dist)) .* (P(:,2) - P(:,1));
+rhoba = U(:,1,1) + ((distin - dist)./(dist2-dist)) .* (U(:,2,1) - U(:,1,1));
+Eba  = E(:,1) + ((distin - dist)./(dist2-dist)) .* (E(:,2) - E(:,1));
 
-Ub1(:,1) = rhoba;
-Ub1(:,2) = rhoba*uba;
-Ub1(:,3) = rhoba*vba;
-Ub1(:,4) = rhoba*Eba;
+UB1(:,1) = rhoba;
+UB1(:,2) = rhoba.*uba;
+UB1(:,3) = rhoba.*vba;
+UB1(:,4) = rhoba.*Eba;
 
 end
