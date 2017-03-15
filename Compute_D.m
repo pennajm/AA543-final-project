a04d3ba501721c647937007e3d0a908b3eb9b4c4
pennajm <pennajm@mit.edu>
2017-03-15 12:55:25 -0700

    Add files via upload
    Fixed to new conventions

--- Compute_D.m
+++ Compute_D.m
@@ -1,21 +1,16 @@
-function [ output_args ] = Untitled13( input_args )
+function [D] = Compute_D( I_MAX, J_MAX, U,S)
 
-Rnbp = u(round(0.25*I_MAX):round(0.75*I_MAX),J_MAX-1,3) + ...
-     + u(round(0.25*I_MAX):round(0.75*I_MAX), J_MAX-1,4) + 2*C_fs/(gamma-1);
-Rnim = u(round(0.25*I_MAX):round(0.75*I_MAX),J_MAX-1,3) + ...
-     +u(round(0.25*I_MAX):round(0.75*I_MAX), J_MAX-1,4) - 2*C_fs/(gamma-1);
-%unbi = (Rnbp + Rnim)/2;
-cbi = (Rnbp - Rnim)*0.25*(gamma-1);
 
 P = (gamma -1)*(U(:,:,4) - 0.5*(U(:,:,2).^2 + U(:,:,3).^2)./U(:,:,1));
-uvec = ones(I_MAX,J_MAX,2);
-uvec(:,:,1) = u_fs; 
-uvec(:,:,2) = v_fs;
+%uvec = ones(I_MAX,J_MAX,2);
+u = U(:,:,2)./U(:,:,1); 
+v = U(:,:,3)./U(:,:,1);
 nu = zeros(I_MAX,1);
 E2 = zeros(I_MAX-1,J_MAX-1,4);
 E4 = zeros(I_MAX-1,J_MAX-1,4);
 k2 = 1/4; 
 k4 = 1/256;
+D = zeros(I_MAX-1,J_MAX-1,4);
 
 for ii = 1:I_MAX-1
  
@@ -31,58 +26,45 @@ for ii = 1:I_MAX-1
     end
 end
 
-for nind = 0:3
+for nind = 1:4
     for ii = 1:I_MAX-1
         for jj = 1:J_MAX-1
-            magds = sqrt(cell_n(ii,jj,(1+nind*2))^2 + cell_n(ii,jj,(2+nind*2))^2);
+            magds = sqrt(S(ii,jj,nind,1)^2 + S(ii,jj,nind,2)^2);
             if ii == 1
-                E2(ii,jj,nind+1) = 0.5*k2*(dot(uvec(ii,jj,:),cell_n(ii,jj,(1+nind*2):(2+nind*2)))...
+                E2(ii,jj,nind) = 0.5*k2*((u(ii,jj)*S(ii,jj,nind,1) + v(ii,jj)*S(ii,jj,nind,2))...
                     + C_fs*magds)*max([nu(I_MAX),nu(ii), nu(ii+1), nu(ii+2)]);
             elseif ii == I_MAX-1
-                E2(ii,jj,nind+1) = 0.5*k2*(dot(uvec(ii,jj,:),cell_n(ii,jj,(1+nind*2):(2+nind*2)))...
+                E2(ii,jj,nind) = 0.5*k2*((u(ii,jj)*S(ii,jj,nind,1) + v(ii,jj)*S(ii,jj,nind,2))...
                     + C_fs*magds)*max([nu(ii-1),nu(ii), nu(ii+1), nu(1)]);
             else
-                E2(ii,jj,nind+1) = 0.5*k2*(dot(uvec(ii,jj,:),cell_n(ii,jj,(1+nind*2):(2+nind*2)))...
+                E2(ii,jj,nind) = 0.5*k2*((u(ii,jj)*S(ii,jj,nind,1) + v(ii,jj)*S(ii,jj,nind,2))...
                     + C_fs*magds)*max([nu(ii-1),nu(ii), nu(ii+1), nu(ii+2)]);
             end
 
-            E4(ii,jj,nind+1) = max([0,(0.5*k4*(dot(uvec(ii,jj,:),cell_n(ii,jj,(1+nind*2):(2+nind*2)))...
-                + C_fs*magds) - E2(ii,jj,nind+1))]);
+            E4(ii,jj,nind) = max([0,(0.5*k4*((u(ii,jj)*S(ii,jj,nind,1) + v(ii,jj)*S(ii,jj,nind,2))...
+                + C_fs*magds) - E2(ii,jj,nind))]);
 
             if ii== 1
-                D(ii,jj,nind+1,:) = E2(ii,jj,nind+1).*(U(ii+1,jj,nind+1,:) - U(ii,jj,nind+1,:)) - ...
-                    (E4(ii,jj,nind+1).*(U(ii+2,jj,nind+1,:) - 3*U(ii+1,jj,nind+1,:)...
-                    + 3*U(ii,jj,nind+1,:) - U(I_MAX-1,jj,nind+1,:)));
+                D(ii,jj,nind) = E2(ii,jj,nind).*(U(ii+1,jj) - U(ii,jj)) - ...
+                    (E4(ii,jj,nind).*(U(ii+2,jj) - 3*U(ii+1,jj)...
+                    + 3*U(ii,jj) - U(I_MAX-1,jj)));
             elseif ii == I_MAX-2
-                D(ii,jj,nind+1,:) = E2(ii,jj,nind+1).*(U(ii+1,jj,nind+1,:) - U(ii,jj,nind+1,:)) - ...
-                    (E4(ii,jj,nind+1).*(U(1,jj,nind+1,:) - 3*U(ii+1,jj,nind+1,:) + ...
-                    3*U(ii,jj,nind+1,:) - U(ii-1,jj,nind+1,:)));
+                D(ii,jj,nind) = E2(ii,jj,nind).*(U(ii+1,jj) - U(ii,jj)) - ...
+                    (E4(ii,jj,nind).*(U(1,jj) - 3*U(ii+1,jj) + ...
+                    3*U(ii,jj) - U(ii-1,jj)));
 
             elseif ii == I_MAX-1
-                D(ii,jj,nind+1,:) = E2(ii,jj,nind+1).*(U(1,jj,nind+1,:) - U(ii,jj,nind+1,:)) - ...
-                    (E4(ii,jj,nind+1).*(U(2,jj,nind+1,:) - 3*U(1,jj,nind+1,:) + ...
-                    3*U(ii,jj,nind+1,:) - U(ii-1,jj,nind+1,:)));
+                D(ii,jj,nind) = E2(ii,jj,nind).*(U(1,jj) - U(ii,jj)) - ...
+                    (E4(ii,jj,nind).*(U(2,jj) - 3*U(1,jj) + ...
+                    3*U(ii,jj) - U(ii-1,jj)));
             else
-                D(ii,jj,nind+1,:) = E2(ii,jj,nind+1).*(U(ii+1,jj, nind+1,:) - U(ii,jj,nind+1,:)) - ...
-                    (E4(ii,jj,nind+1).*(U(ii+2,jj,nind+1,:) - 3*U(ii+1,jj,nind+1,:)...
-                    + 3*U(ii,jj,nind+1,:) - U(ii-1,jj,nind+1,:)));
+                D(ii,jj,nind) = E2(ii,jj,nind).*(U(ii+1,jj) - U(ii,jj)) - ...
+                    (E4(ii,jj,nind).*(U(ii+2,jj) - 3*U(ii+1,jj)...
+                    + 3*U(ii,jj) - U(ii-1,jj)));
             end
         end
     end
 end
-
-
-% %from here assuming the normal boundary conditions
-% U = U_IC; G = G_IC; F = F_IC;
-% %calculate Reimann invariants based on freestream
-% Rnbpi = u_IC(0.25*I_MAX:0.75*I_MAX,J_MAX-1,3) + ...
-%     +u_IC(0.25*I_MAX:0.75*I_MAX, J_MAX-1,4) + 2*cfs/(gamma-1);
-% Rnim = u_IC(0.25*I_MAX:0.75*I_MAX,J_MAX-1,3) + ...
-%     +u_IC(0.25*I_MAX:0.75*I_MAX, J_MAX-1,4) - 2*cfs/(gamma-1);
-% %unbi = (Rnbp + Rnim)/2;
-% cbi = (Rnbp - Rnim)*0.25*(gamma-1);
-% %use streamline constants to calculate interior values
-% Hfs = (gamma/(gamma-1))*Pfs/rhofs + 0.5*ufs^2;
 
 
 end
