be9968a40101086943fb65e71ebb0ddb3beab434
pennajm <pennajm@mit.edu>
2017-03-16 19:56:20 -0700

    Add files via upload
    updates all four components

--- Update_U.m
+++ Update_U.m
@@ -4,7 +4,7 @@ alpha = zeros(4,1);
 alpha(1) = 1/8; alpha(2) = 0.306; alpha(3) = 0.587; alpha(4) = 1;
 for ii = 1:I_MAX-1
     for jj = 1:J_MAX-1
-        U(ii,jj) = U(ii,jj) - TAU(ii,jj).*alpha(RK).*R(ii,jj);
+        U(ii,jj,:) = U(ii,jj,:) - TAU(ii,jj).*alpha(RK).*R(ii,jj,:);
     end
 end
 end
